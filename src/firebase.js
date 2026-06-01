4dce10ed285af3b39ea8ff3af36bf6ac8a2768ca
ali <cfcali2662@gmail.com>
2026-06-01 13:12:17 +0300

    a

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpd7TlI5R_RHHptJ65sRQuqTsQSoBfs4c",
-  authDomain: "iklimlendirme-projesi-5d539.firebaseapp.com",
-  projectId: "iklimlendirme-projesi-5d539",
-  storageBucket: "iklimlendirme-projesi-5d539.firebasestorage.app",
-  messagingSenderId: "535497378355",
-  appId: "1:535497378355:web:f7eb8ccc51451cb1a83146",
+  apiKey: "AIzaSyDjRHWvvcpWcyNe5SHLtW40eEOkJZ4DN_Q",
+  authDomain: "iklimlendirme-projesi-c44c7.firebaseapp.com",
+  projectId: "iklimlendirme-projesi-c44c7",
+  storageBucket: "iklimlendirme-projesi-c44c7.firebasestorage.app",
+  messagingSenderId: "681241970843",
+  appId: "1:681241970843:web:47e3417e2159be4777de3e"
 };
 
 const app = initializeApp(firebaseConfig);
